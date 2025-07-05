ec7c3982525334a334747550d41cbd480b111ea0
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-07-05 22:20:18 +0300

    added row numbers

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,3 +8,9 @@ SELECT property_id, COUNT(*) AS total_bookings,
        RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
 FROM bookings
 GROUP BY property_id;
+
+SELECT 
+    user_id, 
+    id AS booking_id,
+    ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY start_date) AS booking_rank
+FROM bookings;
