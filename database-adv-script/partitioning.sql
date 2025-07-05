f16567b2fa0801ff3918ffb08819e95adc95c8cc
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-07-05 22:31:55 +0300

    added partitionby

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,7 +1,21 @@
--- Partition bookings table by start_date (PostgreSQL example)
-CREATE TABLE bookings_2024 PARTITION OF bookings
-FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
+-- Partition bookings table by RANGE on start_date using declarative partitioning
+CREATE TABLE bookings_partitioned (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    property_id INT,
+    start_date DATE,
+    end_date DATE,
+    status VARCHAR(20)
+) PARTITION BY RANGE (start_date);
+
+-- Create individual partitions
+CREATE TABLE bookings_2023 PARTITION OF bookings_partitioned
+    FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+CREATE TABLE bookings_2024 PARTITION OF bookings_partitioned
+    FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
 
 -- Example query to test partition performance
-SELECT * FROM bookings
+EXPLAIN ANALYZE
+SELECT * FROM bookings_partitioned
 WHERE start_date BETWEEN '2024-01-01' AND '2024-06-30';
