11f97cba19689be0554c8101394750837207b0f8
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-07-05 22:29:09 +0300

    improved performance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -13,4 +13,4 @@ FROM bookings b
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 JOIN payments pay ON b.id = pay.booking_id
-WHERE b.status = 'confirmed' and pay.status = 'paid';
+WHERE b.status = 'confirmed' AND pay.status = 'paid';
